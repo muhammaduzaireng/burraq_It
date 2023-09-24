014ed7df92cf4b335fedfb088c3bcae9efbe70bb
Muhammad Uzair <muzaireng@gmail.com>
2023-09-24 05:29:17 +0500

    Audio - Video done as per requirements and other pages make responsive.

--- Cretech/Cretech/src/components/Services/Services1/Main.js
+++ Cretech/Cretech/src/components/Services/Services1/Main.js
@@ -1,10 +1,144 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import OwlCarousel from 'react-owl-carousel';
 import Breadcumb from '../../Breadcumb/Main'
 import BG from '../../../assets/images/background/cta-two-bg-1-1.jpg'
+import BgImg from  "../../../assets/images/background/page-header-bg-1-1.jpg"
 
 const Service1 = () => {
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+  const imagePaths = [
+    "assets/images/resources/client-1-1.svg",
+    "assets/images/resources/client-1-2.svg",
+    "assets/images/resources/client-1-3.svg",
+    "assets/images/resources/client-1-4.svg",
+    "assets/images/resources/client-1-5.svg",
+    "assets/images/resources/client-1-6.svg",
+    "assets/images/resources/client-1-1.png",
+    "assets/images/resources/Picture1.png",
+    "assets/images/resources/Picture2.png",
+    "assets/images/resources/Picture3.png",
+    "assets/images/resources/Picture4.png",            
+    "assets/images/resources/Picture5.png",
+    "assets/images/resources/Picture6.png",
+    "assets/images/resources/Picture7.png",  
+    "assets/images/resources/Picture8.png",
+    "assets/images/resources/Picture9.png",
+    "assets/images/resources/Picture10.png",
+    "assets/images/resources/Picture11.png",
+    "assets/images/resources/Picture12.png",
+    "assets/images/resources/Picture13.png",
+    "assets/images/resources/Picture14.png",
+    "assets/images/resources/Picture15.png",
+    "assets/images/resources/Picture16.png",
+    "assets/images/resources/Picture17.png",    
+    "assets/images/resources/Picture18.png",
+    "assets/images/resources/Picture20.png", 
+    "assets/images/resources/Picture21.png", 
+    "assets/images/resources/Picture22.png", 
+    "assets/images/resources/Picture23.png", 
+    "assets/images/resources/Picture24.png",  
+    "assets/images/resources/Picture25.png",   
+    "assets/images/resources/Picture26.png",   
+    "assets/images/resources/Picture27.png",   
+    "assets/images/resources/Picture28.png",   
+    "assets/images/resources/Picture29.png",   
+    "assets/images/resources/Picture30.png",   
+    "assets/images/resources/Picture31.png",   
+    "assets/images/resources/Picture32.png",   
+    "assets/images/resources/Picture33.png",   
+    "assets/images/resources/Picture34.png",   
+    "assets/images/resources/Picture35.png",   
+    "assets/images/resources/Picture36.png",   
+    "assets/images/resources/Picture37.png",   
+    "assets/images/resources/Picture38.png",   
+    "assets/images/resources/Picture39.png",   
+    "assets/images/resources/Picture40.png",   
+    "assets/images/resources/Picture41.png",   
+    "assets/images/resources/Picture42.png",   
+    "assets/images/resources/Picture43.png",   
+    "assets/images/resources/Picture44.png",  
+    "assets/images/resources/Picture45.png",
+
+  ];
+
+  const imageData = [
+    {
+      title: 'Image 1',
+      origin: 'German',
+      category: 'Hardware & Software',
+      established: 'Mid 2000s',
+      description: 'Description for Image 1',
+      headerImage: 'assets/images/projects/audinateCover.jpg'
+    },
+    {
+      title: 'Image 1',
+      origin: 'German',
+      category: 'Hardware & Software',
+      established: 'Mid 2000s',
+      description: 'Description for Image 2',
+    },
+    {
+      title: 'Image 1',
+      origin: 'German',
+      category: 'Hardware & Software',
+      established: 'Mid 2000s',
+      description: 'Description for Image 3',
+    },
+    {
+      title: 'Image 1',
+      origin: 'German',
+      category: 'Hardware & Software',
+      established: 'Mid 2000s',
+      description: 'Description for Image 4',
+    },
+    {
+      title: 'Image 1',
+      origin: 'German',
+      category: 'Hardware & Software',
+      established: 'Mid 2000s',
+      description: 'Description for Image 1',
+    },
+    {
+      title: 'Image 1',
+      origin: 'German',
+      category: 'Hardware & Software',
+      established: 'Mid 2000s',
+      description: 'Description for Image 1',
+    },
+    {
+      title: 'Image 1',
+      origin: 'German',
+      category: 'Hardware & Software',
+      established: 'Mid 2000s',
+      description: 'Description for Image 1',
+    },
+    {
+      title: 'Image 1',
+      origin: 'German',
+      category: 'Hardware & Software',
+      established: 'Mid 2000s',
+      description: 'Description for Image 1',
+    },
+    {
+      title: 'Image 1',
+      origin: 'German',
+      category: 'Hardware & Software',
+      established: 'Mid 2000s',
+      description: 'Description for Image 1',
+    },
+    
+    // Add more data objects for each image
+  ];
+  const handleImageClick = (index) => {
+    setSelectedImageIndex(index);
+    setShow(true);
+  };
+
+  const selectedImageData = imageData[selectedImageIndex];
+
+
+  const [show, setShow] = useState (false);
   const logoOptions = {
     loop: true,
     autoplay: true,
@@ -69,9 +203,10 @@ const Service1 = () => {
       }
     }
   }
+  
   return (
     <>
-      <Breadcumb Title="Audio - Video" Breadcumb="Audio - Video" />
+      <Breadcumb Title="Audio - Video" Breadcumb="Audio - Video"  backgroundImage={BgImg} />
       <section className="section-padding--top section-padding--bottom about-one">
         <div className="container">
           <div className="row gutter-y-60">
@@ -116,163 +251,68 @@ const Service1 = () => {
       <div className="client-carousel client-carousel--two">
         <div className="container">
           <OwlCarousel className="thm-owl__carousel" {...logoOptions}>
-            <div className="item">
-              <img src="assets/images/resources/client-1-1.svg" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/client-1-2.svg" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/client-1-3.svg" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/client-1-4.svg" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/client-1-5.svg" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/client-1-6.svg" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/client-1-1.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture1.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture2.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture3.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture4.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture5.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture6.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture7.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture8.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture9.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture10.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture11.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture12.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture13.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture14.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture15.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture16.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture17.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture18.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture20.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture21.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture22.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture23.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture24.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture25.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture26.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture27.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture28.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture29.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture30.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture31.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture32.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture33.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture34.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture35.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture36.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture37.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture38.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture39.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture40.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture41.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture42.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture43.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture44.png" alt="" />
-            </div>
-            <div className="item">
-              <img src="assets/images/resources/picture45.png" alt="" />
-            </div>
+          {imagePaths.map((imagePath, index) => (
+          <div className="item" key={index}>
+            <img
+              onClick={() => handleImageClick(index)}
+              src={imagePath}
+              alt={`Image ${index + 1}`}
+            />
+          </div>
+        ))}
+            
 
           </OwlCarousel>
         </div>
       </div>
+      
+      
+      {show && ( 
+        <section className="project-details section-padding--bottom">
+				<div className="container">
+					<div className="project-details__image">
+						<img src={selectedImageData.headerImage} alt="" />
+					</div>
+					<div className="project-details__info">
+						<div className="project-details__info__item">
+							<h4 className="project-details__info__title">Origin:</h4>
+							<p className="project-details__info__text">{selectedImageData.origin}</p>
+						</div>
+
+						<div className="project-details__info__item">
+							<h4 className="project-details__info__title">Category:</h4>
+							<p className="project-details__info__text"><Link to="#">{selectedImageData.category}</Link></p>
+
+						</div>
+						<div className="project-details__info__item">
+							<h4 className="project-details__info__title">Established:</h4>
+							<p className="project-details__info__text">{selectedImageData.established}</p>
+						</div>
+						
+						<div className="project-details__info__item">
+							<div className="project-details__info__social">
+								<Link to="#"><i className="fab fa-twitter"></i></Link>
+								<Link to="#"><i className="fab fa-facebook"></i></Link>
+								<Link to="#"><i className="fab fa-instagram"></i></Link>
+							</div>
+						</div>
+					</div>
+					<h3 className="project-details__title">Manufacturing Specialist</h3>
+					<div className="project-details__content">
+          <p>
+            {selectedImageData.description}
+          </p>
+						<ul className="project-details__list">
+							<li>
+								<i className="fa fa-check-circle"></i>
+								LED Tubes, Pars and Bulbs, DMX Box and App.
+							</li>
+							
+						</ul>
+					</div>
+				</div>
+			</section>
+)}  
       <section className="black-bg section-padding-lg--top section-padding-lg--bottom cta-two">
         <div className="cta-two__bg jarallax" data-jarallax data-speed="0.2" data-imgPosition="50% 0%"
           style={{ backgroundImage: `url(${BG})` }}></div>
